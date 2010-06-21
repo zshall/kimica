854ba662d9fd673d3632ae1358af5d55a5df9972
seinoxygen <seinoxygen@hotmail.com>
2010-06-21 00:55:23 -0300

    reverted

--- ext/upgrade/main.php
+++ ext/upgrade/main.php
@@ -33,9 +33,8 @@ class Upgrade extends SimpleExtension {
 		if($config->get_int("db_version") < 8) {
 			// if this fails, don't try again
 			$config->set_int("db_version", 8);
-			$database->execute($database->engine->scoreql_to_sql(
-				"ALTER TABLE images ADD COLUMN locked SCORE_BOOL NOT NULL DEFAULT SCORE_BOOL_N"
-			));
+			$database->execute($database->engine->scoreql_to_sql("ALTER TABLE images ADD COLUMN status ENUM('l', 'a', 'p', 'd') NOT NULL DEFAULT 'p'"));
+			$database->execute($database->engine->scoreql_to_sql("ALTER TABLE users ADD COLUMN validate VARCHAR(16) NULL"));
 			log_info("upgrade", "Database at version 8");
 		}
 	}
