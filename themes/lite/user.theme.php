c3319953ad269df48c552cda0788546877c6208b
seinoxygen <seinoxygen@hotmail.com>
2010-06-22 23:29:19 -0300

    added recover link on themes

--- themes/lite/user.theme.php
+++ themes/lite/user.theme.php
@@ -24,6 +24,7 @@ class CustomUserPageTheme extends UserPageTheme {
 		if($config->get_bool("login_signup_enabled")) {
 			$html .= "<small><a href='".make_link("account/create")."'>Create Account</a></small>";
 		}
+		$html .= "&nbsp;<small><a href='".make_link("account/recover")."'>Recover Password</a></small>";
 		$page->add_block(new Block("Login", $html, "main", 90));
 	}
 
