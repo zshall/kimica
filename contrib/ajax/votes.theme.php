9a6afa675cd2bc850a9a21ee1b5e9e42bebfe02f
seinoxygen <seinoxygen@hotmail.com>
2010-07-08 23:49:19 -0300

    votes ajaxed

---
+++ contrib/ajax/votes.theme.php
@@ -0,0 +1,17 @@
+<?php
+class CustomVotesTheme extends VotesTheme {
+	public function get_voter_html(Image $image) {
+		$i_score = int_escape($image->votes);
+
+		$html = "
+			Current Score: $i_score
+
+			<p><input type='submit' onclick='PostVote($image->id,\"up\");' value='Vote Up'>
+			<br>
+			<input type='submit' onclick='PostVote($image->id,\"down\");' value='Vote Down'>
+			</p>
+		";
+		return $html;
+	}
+}
+?>
