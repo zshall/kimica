d3a013c4ac1c9aaa0541a495b4596efc59344903
seinoxygen <seinoxygen@hotmail.com>
2010-07-10 18:27:37 -0300

    change from onclick to click event

--- contrib/ajax/favorite.theme.php
+++ contrib/ajax/favorite.theme.php
@@ -3,11 +3,12 @@ class CustomFavoritesTheme extends FavoritesTheme {
 	public function get_voter_html(Image $image, $is_favorited) {
 		global $page, $user;
 		
+		$html = "<input id='image_id' type='hidden' value='$image->id' name='image_id'>";
 		if(!$is_favorited) {
-			$html = "<input id='post-favorite' onclick='Post.Favorite($image->id,\"set\");' type='submit' value='Favorite'>";
+			$html .= "<input id='post-favorite' type='submit' value='Favorite'>";
 		}
 		else {
-			$html = "<input id='post-favorite' onclick='Post.Favorite($image->id,\"unset\");' type='submit' value='Un-Favorite'>";
+			$html .= "<input id='post-favorite' type='submit' value='Un-Favorite'>";
 		}
 		
 		return $html;
