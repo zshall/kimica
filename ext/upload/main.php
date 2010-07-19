b82f81bf7cab5d8e29fc74afa5dbdbdcf2b56ff6
seinoxygen <seinoxygen@hotmail.com>
2010-07-19 20:36:59 -0300

    added warehoused event to update image table

--- ext/upload/main.php
+++ ext/upload/main.php
@@ -32,6 +32,14 @@ class DataUploadEvent extends Event {
 	}
 }
 
+class DataWarehousedEvent extends Event {
+	var $hash;
+
+	public function DataWarehousedEvent($hash) {
+		$this->hash = $hash;
+	}
+}
+
 class UploadException extends SCoreException {}
 
 class Upload implements Extension {
@@ -46,6 +54,11 @@ class Upload implements Extension {
 			$config->set_default_int('upload_size', '1MB');
 			$config->set_default_bool('upload_anon', false);
 			$config->set_default_string('upload_autoapprove', 'oam');
+		}
+		
+		if($event instanceof DataWarehousedEvent) {
+			$image = Image::by_hash($event->hash);
+			$image->set_warehoused();
 		}
 
 		if($event instanceof PostListBuildingEvent) {
@@ -362,7 +375,16 @@ class Upload implements Extension {
 		$event = new DataUploadEvent($user, $tmpname, $metadata);
 		send_event($event);
 		if($event->image_id == -1) {
-			throw new UploadException("1 File type not recognised");
+			throw new UploadException("File type not recognised");
+		}
+		else{
+			//If it was uploaded sucessfully then set as warehoused.
+			$backup_method = $config->get_string('warehouse_method','local_hierarchy');
+			$methods = explode("_",$backup_method);
+			
+			if(in_array('amazon', $methods)){
+				send_event(new DataWarehousedEvent($event->hash));
+			}
 		}
 	}
 	
