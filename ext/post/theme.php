0db69f75eaa1ce68c0d4201794616a501a0f4053
seinoxygen <seinoxygen@hotmail.com>
2010-08-19 17:33:08 -0300

    fixed url profile

--- ext/post/theme.php
+++ ext/post/theme.php
@@ -215,7 +215,7 @@ class PostTheme extends Themelet {
 
 	private function build_stats($image) {
 		$h_owner = html_escape($image->get_owner()->name);
-		$h_ownerlink = "<a href='".make_link("user/$h_owner")."'>$h_owner</a>";
+		$h_ownerlink = "<a href='".make_link("account/profile/$h_owner")."'>$h_owner</a>";
 		$h_ip = html_escape($image->owner_ip);
 		$h_date = autodate($image->posted);
 		$h_filesize = to_shorthand_int($image->filesize);
